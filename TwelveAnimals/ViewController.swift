e78ac3ad59f9ace2b785c9e3dfb3e654ec190445
guoweidong <guoweidong@guoweidongs-MacBook-Pro.local>
2016-04-13 13:17:58 +0800

    add image for Animals

--- TwelveAnimals/ViewController.swift
+++ TwelveAnimals/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var year: UITextField!
+    
+    @IBOutlet weak var image: UIImageView!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func enter() {
+        year.resignFirstResponder()
+        if let yearNumber = Int(year.text!) {
+            let imageIndex = (yearNumber - 4) % 12
+            image.image = UIImage(named: "\(imageIndex)")
+        }
+        else {
+            print("你输的是啥啊")
+        }
     }
-
+    // 点击window其他任意位置，键盘消失
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        year.resignFirstResponder()
+    }
+    
+    
 
 }
 
